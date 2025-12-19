48cc6dec433b2107d78d0a67875a9ff168637d1d
Einar <einari@me.com>
2025-12-19 18:36:55 +0100

    Adding args

--- Source/Dialogs/ConfirmationDialog.stories.tsx
+++ Source/Dialogs/ConfirmationDialog.stories.tsx
@@ -11,8 +11,8 @@ const meta: Meta<typeof ConfirmationDialog> = {
 export default meta;
 type Story = StoryObj<typeof ConfirmationDialog>;
 
-const DialogWrapper = () => {
-    const [showDialog] = useConfirmationDialog('Are you sure you want to proceed?', 'Do you really really want to proceed with this action?', DialogButtons.YesNoCancel);
+const DialogWrapper = ({ title, message }: { title: string; message: string }) => {
+    const [showDialog] = useConfirmationDialog(title, message, DialogButtons.YesNoCancel);
     return (
         <button onClick={showDialog}>Show dialog</button>
     )
@@ -21,13 +21,13 @@ const DialogWrapper = () => {
 
 export const Default: Story = {
     args: {
-        title: 'Loading',
-        message: 'Please wait while we process your request...'
+        title: 'Are you sure you want to proceed?', 
+        message: 'Do you really really want to proceed with this action?'
     },
     render: (args) => (
         <div div className="storybook-wrapper" >
             <DialogComponents confirmation={ConfirmationDialog}>
-                <DialogWrapper />
+                <DialogWrapper title={args.title} message={args.message} />
             </DialogComponents>
         </div>
     )
