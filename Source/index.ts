30d8c406074fbb6bf5e93b9cd71f133e06bc8719
Einar <einari@me.com>
2026-01-06 09:00:37 +0100

    Adding missing index

---
+++ Source/index.ts
@@ -0,0 +1,22 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import * as CommandDialog from './CommandDialog';
+import * as CommandForm from './CommandForm';
+import * as DataPage from './DataPage';
+import * as dataTables from './DataTables';
+import * as dialogs from './Dialogs';
+import * as Dropdown from './Dropdown';
+import * as PivotViewer from './PivotViewer';
+import * as TimeMachine from './TimeMachine';
+
+export {
+    CommandDialog,
+    CommandForm,
+    DataPage,
+    dataTables,
+    dialogs,
+    Dropdown,
+    PivotViewer,
+    TimeMachine,
+};
