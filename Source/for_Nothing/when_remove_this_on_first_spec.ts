9e3ea381dd92633e970524e531474983f53b3ff5
Einar <einari@me.com>
2026-01-11 08:12:33 +0100

    Adding a temp test till we have specs

---
+++ Source/for_Nothing/when_remove_this_on_first_spec.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+describe('when remove this on first spec', () => {
+    it('should still run', () => {
+        true.should.be.true;
+    });
+});
