0b90a7e65ee914637d9eeb54d8e4f9ceebe64246
Einar <einari@me.com>
2026-02-22 13:25:28 +0100

    Fix CSS module declarations using triple-slash directive

--- Source/global.d.ts
+++ Source/global.d.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import * as React from 'react';
+/// <reference types="react" />
 
 declare global {
   namespace JSX {
