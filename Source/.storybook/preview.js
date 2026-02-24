b5b26ad961b11d7bd3b3dc7547b374906b91ea97
Einar <einari@me.com>
2026-02-24 21:32:12 +0100

    Fixing styling

--- Source/.storybook/preview.js
+++ Source/.storybook/preview.js
@@ -1,12 +1,17 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import 'primereact/resources/themes/lara-dark-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import './preview.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: { expanded: true }
+  controls: { expanded: true },
+  backgrounds: {
+    default: 'dark',
+    values: [
+      { name: 'dark', value: '#111827' },
+      { name: 'surface-card', value: '#1f2937' },
+    ],
+  },
 };
