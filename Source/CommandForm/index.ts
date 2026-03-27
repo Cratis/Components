cf256feb3a547d9573200a32c3a871700745727c
Einar <einari@me.com>
2026-03-27 07:58:44 +0100

    Removing re-exporting

--- Source/CommandForm/index.ts
+++ Source/CommandForm/index.ts
@@ -1,25 +1,4 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-// Re-export CommandForm functionality from @cratis/arc.react
-export {
-    CommandForm,
-    CommandFormField,
-    ValidationMessage,
-    CommandFormFields,
-    asCommandFormField,
-    useCommandFormContext,
-    useCommandInstance,
-    useSetCommandResult,
-    type CommandFormProps,
-    type CommandFormFieldProps,
-    type CommandFormFieldComponentProps,
-    type BaseCommandFormFieldProps,
-    type WrappedFieldProps,
-    type CommandFormFieldConfig,
-    type InjectedCommandFormFieldProps,
-    type ColumnInfo
-} from '@cratis/arc.react/commands';
-
-// Export our PrimeReact-based field implementations
 export * from './fields';
