bb5a722db8a324f85ca4d2ef7c838d207c8b216c
woksin <sindrewilting@gmail.com>
2026-09-26 08:11:27 +0200

    Expose ToolbarButton active state to assistive technology

---
+++ Source/Toolbar/for_ToolbarButton/when_rendering_active_state/without_active.tsx
@@ -0,0 +1,17 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { renderToStaticMarkup } from 'react-dom/server';
+import { ToolbarButton } from '../../ToolbarButton';
+
+describe('when rendering a ToolbarButton without active', () => {
+    let html: string;
+
+    beforeEach(() => {
+        html = renderToStaticMarkup(<ToolbarButton title='Draw' />);
+    });
+
+    it('should remain an action without a pressed state', () => {
+        html.should.not.include('aria-pressed');
+    });
+});
